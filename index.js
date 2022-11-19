7d2483dc0cbf3806fc94dc0bf8ef7bdfa2d13c5f
proudCobolWriter <77924245+proudCobolWriter@users.noreply.github.com>
2022-11-19 21:15:23 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,22 +2,32 @@ require("dotenv").config({path : ".env"});
 
 const fs = require("fs");
 const { REST } = require("@discordjs/rest");
-const { Client, GatewayIntentBits, Routes, EmbedBuilder, ActivityType } = require("discord.js");
+const { Client, GatewayIntentBits, Routes, EmbedBuilder, ActivityType, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require("discord.js");
 
 const client = new Client({ intents: 36481 });
 const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
 
+// Constants
+
 const filePath = 'db.json';
 const intervalDelayInSec = 15;
-const commandCooldownInSec = 7;
+const commandCooldownInSec = 10;
+const coooldownMessagesInSec = 30;
+
+// Data storage
 
 let data = [];
-let changed = false;
+let msgCooldownData = new Map();
 
+// Command states
+
+let changed = false;
 let nwordusages = 0;
 
 let cooldownCommand1 = 0;
 let cooldownCommand2 = 0;
+
+// Create user object
 
 function Post(userID, score) {
     this.user = {
@@ -48,7 +58,6 @@ function writeToJSON() {
         } else {
             console.log("The file was successfully written");
             changed = false;
-            nwordusages = nwordusages;
         };
     });
 };
@@ -82,7 +91,9 @@ client.on("messageCreate", (msg) => {
         let userData = data.find(element => element.user && element.user.userID == msg.author.id);
 
         if (userData) {
+            if (msgCooldownData.has(msg.author.id) && msgCooldownData.get(msg.author.id) >= (Date.now() - coooldownMessagesInSec * 1000) ) return;
             userData.user.score += 1;
+            msgCooldownData.set(msg.author.id, Date.now());
 
             // Leveling up related messages
             try {
@@ -120,6 +131,26 @@ client.on("messageCreate", (msg) => {
     };
 });
 
+function milestoneFunction() {
+    if (nwordusages % 1000 == 0 && nwordusages != 0) {
+        try {
+            let guild = client.guilds.cache.get(process.env.GUILD_ID.toString());
+            let createEmbed = () => {
+                return new EmbedBuilder()
+                .setTitle(`The ${nwordusages.toString()} niggas milestone has been reached!`)
+                .setThumbnail(guild.iconURL ? guild.iconURL() : "")
+                .setTimestamp()
+                .setColor("Aqua")
+                .setDescription("Keep it up guys");
+            };
+
+            let firstChannel = guild.channels.cache.find(ch => ch.name.includes("general"));
+
+            firstChannel.send({ embeds: [ createEmbed() ] });
+        } catch (err) { console.log(err); };
+    };
+};
+
 (() => {
     data = readJSON();
     if (!data || data.length == 0) { throw "Data is wrong" };
@@ -141,6 +172,7 @@ client.on("messageCreate", (msg) => {
                 updateNwordUsages();
                 console.log("Updated, n-word counter: " + nwordusages);
                 setDiscordPresence();
+                milestoneFunction();
             };
         } catch (err) { console.log(err); };
     }, intervalDelayInSec * 1000, writeToJSON);
@@ -157,34 +189,75 @@ client.on("interactionCreate", (interaction) => {
             return (b.user.score || 0) - (a.user.score || 0)
         }); // returns the sorted array
 
-        let fields = [];
-        for (let i = 0; i < 5; i++) {
-            let datai = newData[i];
-
-            if (datai && datai.user) {
-                let emoji = i == 0 ? "🥇" : (i == 1 ? "🥈" : (i == 2 ? "🥉" : ""));
-                fields.push({ name: `${emoji} ${i + 1}# place`, value: `<@!${datai.user.userID.toString()}> has recorded exactly **${datai.user.score.toString()}** n-word usages` });
-            } else { break };
-        };
-
         let dataOnThisUser = newData.find((element) => {
             if (!element.user) return false;
             return element.user.userID == interaction.user.id
         });
 
-        let embed = new EmbedBuilder()
-        .setTitle("N-word leaderboard 🏆")
-        .setAuthor({
-            name: interaction.user.tag,
-            iconURL: interaction.user.avatarURL(),
-        })
-        .setFooter({ text: `Please use this command again in ${commandCooldownInSec}s   -   You have said the n-word ${dataOnThisUser && dataOnThisUser.user ? dataOnThisUser.user.score.toString() : "0"} time${dataOnThisUser.user.score && dataOnThisUser.user.score > 1 ? "s" : ""}`, iconURL: client.user.defaultAvatarURL })
-        .setTimestamp()
-        .setColor("Blurple")
-        .addFields(fields)
-        .setDescription("\n");
+        let getFields = (page) => {
+            let fields = [];
 
-        interaction.reply({ embeds: [embed] }).catch(err => console.log("Couldn't send embed on command 1"));
+            for (let i = 5 * (page - 1); i < 5 * page; i++) {
+                let datai = newData[i];
+    
+                if (datai && datai.user) {
+                    let emoji = i == 0 ? "🥇" : (i == 1 ? "🥈" : (i == 2 ? "🥉" : ""));
+                    fields.push({ name: `${emoji} ${i + 1}# place`, value: `<@!${datai.user.userID.toString()}> has recorded exactly **${datai.user.score.toString()}** n-word usages` });
+                } else { break };
+            };
+
+            return fields;
+        };
+
+        let createEmbed = (page, f) => {
+            return new EmbedBuilder()
+            .setTitle(`N-word leaderboard 🏆      (page #${page})`)
+            .setAuthor({
+                name: interaction.user.tag,
+                iconURL: interaction.user.avatarURL(),
+            })
+            .setFooter({ text: `Please use this command again in ${commandCooldownInSec}s   -   You have said the n-word ${dataOnThisUser && dataOnThisUser.user ? dataOnThisUser.user.score.toString() : "0"} time${dataOnThisUser.user.score && dataOnThisUser.user.score > 1 ? "s" : ""}`, iconURL: client.user.defaultAvatarURL })
+            .setTimestamp()
+            .setColor("Blurple")
+            .addFields(f)
+            .setDescription("\n");
+        };
+
+        let createActionRow = (buttonToDisable) => {
+            return new ActionRowBuilder().setComponents(
+                new ButtonBuilder()
+                    .setCustomId("button1")
+                    .setLabel("Page left")
+                    .setEmoji('⬅️')
+                    .setStyle(ButtonStyle.Success)
+                    .setDisabled(buttonToDisable == "button1"),
+                new ButtonBuilder()
+                    .setCustomId("button2")
+                    .setLabel("Page right")
+                    .setEmoji('➡️')
+                    .setStyle(ButtonStyle.Success)
+                    .setDisabled(buttonToDisable == "button2")
+            );
+        };
+
+        const collector = interaction.channel.createMessageComponentCollector({ time: 60*1000 });
+
+        interaction.reply({ embeds: [createEmbed(1, getFields(1))], components: [createActionRow("button1")] }).catch(err => console.log("Couldn't send embed on command 1 : ", err)).finally(() => {
+
+            try {
+                collector.on('collect', async i => {
+                    let e = i.message.embeds[0];
+                    let page = parseInt(e.data.title ? e.data.title.replace(/[^0-9]/g, "") : 1);
+                    let buttonClicked = i.customId;
+                    let updatedPageValue = page + (buttonClicked == "button1" ? -1 : 1);
+                    let buttonToGreyOut = updatedPageValue > 1 ? "none" : "button1"
+                    if (getFields(updatedPageValue + 1).length == 0 && buttonToGreyOut != "button1") buttonToGreyOut = "button2";
+
+                    await i.update({ embeds: [createEmbed(updatedPageValue, getFields(updatedPageValue))], components: [createActionRow(buttonToGreyOut)] });
+                });
+            } catch (err) { console.log("Error occured at button collector : ", err); }
+            
+        });
     } else if (interaction.commandName == "rranking") {
         if (cooldownCommand2 >= (Date.now() - commandCooldownInSec * 1000) && interaction.channelId != process.env.BOT_COMMS_CHANNEL_ID) { console.log("attention cooldown"); return };
         cooldownCommand2 = Date.now();
