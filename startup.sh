08b4e3244070f7b69c3ee40485ff0e15cdba9c81
Proud Cobol Writer <77924245+proudCobolWriter@users.noreply.github.com>
2023-03-11 23:58:42 +0100

    Edited startup bash file

--- startup.sh
+++ startup.sh
@@ -1,6 +1,14 @@
-# Startup sequence
+# Sync the files with the repo
 git clean  -d  -f .
 git fetch
 git pull
+
+# Install node_modules in case it is missing
 npm install
+
+# Print outdated packages
+echo -e "\033[0;31mOUTDATED \033[0mPACKAGES LIST:"
+npm outdated
+
+# Startup sequence
 NODE_ENV=production node .
